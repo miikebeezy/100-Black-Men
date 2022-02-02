b992a7e7284aade31e834fd3f1b31554a69acd2c
miikebeezy <bmykal@aol.com>
2022-02-02 00:08:39 -0500

    Initial commit

---
+++ MyProgram.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+print("Now some fun!")
+input("Enter your name:")
+
+print("How old am I?")
+input("Enter my age: hint: 17")
+
+print("CORRECT-O I am 17 years old you get free high five lol!")
+
+
+
+print("Thank you so much for your time!")
